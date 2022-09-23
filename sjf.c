06f9d7be7fb9c9f556a3e2f71ba1a2c46664c6ea
204g1a05a5 <112166475+204g1a05a5@users.noreply.github.com>
2022-09-23 16:10:46 +0530

    my file is created

---
+++ sjf.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.supraja
+   Roll No: 204G1A05A5
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 23-09-2022
+   Date of Execution: 23-09-2022
+*/
